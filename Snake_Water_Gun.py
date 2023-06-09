aa41061703384856ec411fd09c5d263a462770bf
Suraj Kumar Sah <102317964+SurajSah776@users.noreply.github.com>
2023-06-09 14:27:02 +0600

    updating the code

--- Snake_Water_Gun.py
+++ Snake_Water_Gun.py
@@ -1,9 +1,4 @@
 # Snake Water Gun
-'''Snake, Water and Gun is a variation of the children's game "rock- paper-scissorsii where players use hand gestures to represent a snake, water, or a gun. 
-        *** Rules: ***
--->The gun beats the snake, the water beats the gun, and the snake beats the water. Write a python program to create a Snake Water Gun game in Python using if-else statements.
-Do not create any fancy GUI. Use proper functions to check for win.'''
-
 
 import random
 
